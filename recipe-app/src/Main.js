6f43cf4a12b34549a2d42af362e585008eeaa5a3
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2021-09-27 09:09:51 -0500

    Code outline for MVP steps i-iii

---
+++ recipe-app/src/Main.js
@@ -0,0 +1,30 @@
+// Integrate Edamam API, import
+
+/**
+ * Main class
+ * 
+ * @author Katelyn Breen, created September 2021
+ */
+
+//Main 
+    //run
+
+//returnRecipe (input)
+    //returns single recipe with the given parameters
+
+/**
+ * 
+ */
+//applyTimeframe(amount of recipes)
+    //Create recipeList
+    //for(i=0;i++;recipeAmt)
+        //recipe=returnRecipe
+        //add recipe to recipeList
+    //return recipeList
+
+//returnGroceryList
+    //create groceryList
+    //loop through recipeList
+        //extract ingredients from recipe
+        //add ingredient to groceryList
+    //return groceryList

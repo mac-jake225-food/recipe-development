df57f68294f88cff40b230ed1bf4eb33dc6b8ec7
EmilianoHuerta <70651959+EmilianoHuerta@users.noreply.github.com>
2021-09-29 14:07:23 -0500

    adding in appContainer for testing, will route appropriately when finished

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,12 +1,28 @@
 import NavBar from './NavBar';
+import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AccountBox } from './signup';
+
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
       <Router>
-        <NavBar /> 
+        {/* <NavBar />  */}
+        <AppContainer>
+          <AccountBox />
+        </AppContainer>
+        
+        
       </Router>
-      
   );
 }
 

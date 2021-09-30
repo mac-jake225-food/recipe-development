e0c906c7be63e588c62c309e0de052b720fbbe76
Emiliano Huerta <70651959+EmilianoHuerta@users.noreply.github.com>
2021-09-30 14:23:38 -0500

    Update index.jsx
    adding in in textBox container header for welcome page and small text that will function as a sign in link --padding has been corrected and should appear normal on all device screen sizes

--- recipe-app/src/signup/index.jsx
+++ recipe-app/src/signup/index.jsx
@@ -1,9 +1,10 @@
 import react from 'react'; 
 import styled from 'styled-components'; 
 
+
 const BoxContainer = styled.div`
-  width: 21%;
-  min-height: 75%;
+  width: 280px;
+  min-height: 550px;
   display: flex;
   flex-direction: column;
   border-radius: 19px;
@@ -30,18 +31,52 @@ const BackDrop = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 50%;
+  {/* transform shifts our graident container bar 60 degrees*/} 
   transform: rotate(60deg);
-  top: -290px;
-  left: -70px;
+  {/* informs where to start the gradient */} 
+  top: -270px;
+  left: -90px;
+  {/* gradient blue color*/} 
   background: rgb(2,0,36);
   background: linear-gradient(58deg, rgba(2,0,36,1) 0%, rgba(193,225,193,1) 35%, rgba(0,212,255,1) 100%);
   );
 `;
+const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 14%; 
+`;
+
+const HeaderText = styled.h2`
+  height: 60px; 
+  font-size: 30px;
+  font-weight: 800;
+  line-height: 1px;
+  color: #fff;
+  z-index: 10;
+  margin: 0;
+
+`;
+
+const SmallText = styled.div`
+  color: #fff; 
+  font-weight: 600; 
+  font-size: 10px; 
+  z-index: 10;
+  margin:0; 
+`
+
 
     export function AccountBox(props) {
         return <BoxContainer>
                     <TopContainer> 
                     <BackDrop/>
+                    <HeaderContainer>
+                      <HeaderText>Welcome</HeaderText>
+                      <HeaderText>Back!</HeaderText>
+                      <SmallText>Sign in to continue</SmallText>
+                    </HeaderContainer>
                     </TopContainer>
             </BoxContainer>
     }

41ee75d2de2d6704f0d1a285c0d12ffafeb8c43e
Emiliano Huerta <70651959+EmilianoHuerta@users.noreply.github.com>
2021-09-30 19:13:53 -0500

    creating individual pages

---
+++ recipe-app/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>Hello, this is your home screen</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ recipe-app/src/pages/Recipes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Recipes = () => {
+    return (
+        <div>
+            <h1>Hello, This is your Recipes Page</h1>
+        </div>
+    )
+}
+
+export default Recipes

abaf2c659d4d8e566a8b18e920f64fadedd585df
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2021-10-05 16:52:36 -0500

    added notes to main

--- recipe-app/src/Main.js
+++ recipe-app/src/Main.js
@@ -1,4 +1,5 @@
-// Integrate Edamam API, import
+// import { UserProfile } from "./UserProfile";
+import SpoonacularApi from './spoonacular';
 
 /**
  * Main class
@@ -6,43 +7,71 @@
  * @author Katelyn Breen, created September 2021
  */
 
-//Main 
-    //run
+class Main{
+    main(){
+        //returnRecipe (input)
+        //returns single recipe with the given parameters
+            //prepTime
+            //Cuisine
+            //budget
+            //nutrition
+            //Type of dish(dinner,lunch, etc.)
+            //Allergies/foods to avoid
+        //will need to look at api documentation more before outlining steps
+        //write functions for each parameter that can be called in this function
 
-//returnRecipe (input)
-    //returns single recipe with the given parameters
-        //prepTime
-        //Cuisine
-        //budget
-        //nutrition
-        //Type of dish(dinner,lunch, etc.)
-        //Allergies/foods to avoid
-    //will need to look at api documentation more before outlining steps
-    //write functions for each parameter that can be called in this function
+        /**
+         * returns n recipes that the user has selected for their timeframe
+         */
+        //recipeList(n)
+            //Create recipeList
+            //for(i=0;i++;n)
+                //recipe=returnRecipe(input)
+                //add recipe to recipeList
+            //return recipeList
 
-/**
- * returns n recipes that the user has selected for their timeframe
- */
-//applyTimeframe(n)
-    //Create recipeList
-    //for(i=0;i++;n)
-        //recipe=returnRecipe(input)
-        //add recipe to recipeList
-    //return recipeList
+        //groceryList(recipeList)
+            //create groceryList
+            //loop through recipeList
+                //extract ingredients from recipe
+                //add ingredient to groceryList
+            //return groceryList
 
-//returnGroceryList(recipeList)
-    //create groceryList
-    //loop through recipeList
-        //extract ingredients from recipe
-        //add ingredient to groceryList
-    //return groceryList
+        //Calendar functions?
 
-//Calendar functions?
-    //interpolated variable
-//Confirming or denying the output recipes?
+        //Store User info: database
 
-//call website
-    //api key as password
 
-// create user object
-//Confirming or denying the output recipes?
+        //template to run functions:
+        // function add(a, b) {
+        //     return a+b
+        //     }
+        // console.log(add(4, 6))
+        //In terminal, direct to src, and enter node Main.js
+
+        /**
+         * test
+         */
+        let userProfileTest = new UserProfile("tenDays", "tenMin", "italian", "tenDollars", "highProtein", "dinner");
+
+        console.log(userProfileTest.budget);
+        userProfileTest.setBudget("elevenDollars");
+        console.log(userProfileTest.budget);
+        console.log(userProfileTest.getBudget());
+        //bug: User profile not defined, won't return data
+    }   
+}
+
+
+// let userProfileTest = new UserProfile("tenDays", "tenMin", "italian", "tenDollars", "highProtein", "dinner");
+
+// console.log(userProfileTest.budget);
+// userProfileTest.setBudget("elevenDollars");
+// console.log(userProfileTest.budget);
+// console.log(userProfileTest.getBudget());
+
+
+//Questions:
+// instantiate userProfile
+// Database?
+

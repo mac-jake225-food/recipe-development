148bb18e01ea9d1034cdd56bc16efcb87d1021d3
abigailstolp <astolp@macalester.edu>
2021-10-11 12:31:52 -0500

    Calendar Photo added

--- recipe-app/src/pages/Calendar.js
+++ recipe-app/src/pages/Calendar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CalanderPic from './CalendarPic.png';
 
 const Calendar = () => {
   return (
@@ -10,6 +11,7 @@ const Calendar = () => {
         height: '90vh'
       }}
     >
+      <img src={CalanderPic} height={600} width={1100}/>
       <h1>Calendar</h1>
     </div>
   );

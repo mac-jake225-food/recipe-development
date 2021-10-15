81f38db7f890e394f84bc0e509e323c19d0321d6
EmilianoHuerta <70651959+EmilianoHuerta@users.noreply.github.com>
2021-10-15 10:25:27 -0500

    fixing sidebar open/closing functions, implementing css for icon rotation, and removing navbar color

--- recipe-app/src/SideBar/Sidebar.js
+++ recipe-app/src/SideBar/Sidebar.js
@@ -1,20 +1,34 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
+import {BsFillArrowLeftCircleFill} from "react-icons/bs";
+import { IconContext } from 'react-icons';
+
 import './Sidebar.css';
 
 //rafc
 
+
 export const Sidebar = () => {
     const [sidebar, setSidebar] = useState(false);
-
+  
     const showSidebar = () => setSidebar(!sidebar);
+  
     return (
-        <>
+      <>
+        <IconContext.Provider value={{ color: 'black' }}>
           <div className='sidebar'>
+            <Link to='#' className='menu-bars'>
+              <BsFillArrowLeftCircleFill onClick={showSidebar} />
+            </Link>
           </div>
-          <nav className={Sidebar ? 'sidebar-menu active' : 'sidebar-menu'}>
+          <nav className={sidebar ? 'sidebar-menu active' : 'sidebar-menu'}>
             <ul className='sidebar-menu-items' onClick={showSidebar}>
+              <li className='sidebar-toggle'>
+                <Link to='#' className='menu-bars'>
+                  <BsFillArrowLeftCircleFill/>
+                </Link>
+              </li>
               {SidebarData.map((item, index) => {
                 return (
                   <li key={index} className={item.cName}>
@@ -27,8 +41,8 @@ export const Sidebar = () => {
               })}
             </ul>
           </nav>
+        </IconContext.Provider>
       </>
     );
 }
-
 export default Sidebar;

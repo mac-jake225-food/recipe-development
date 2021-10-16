01921a667ec88273440d6b5a333294df950f41c2
EmilianoHuerta <70651959+EmilianoHuerta@users.noreply.github.com>
2021-10-16 18:26:34 -0500

    adding in home stock images and text

--- recipe-app/src/Pages/HomePage/Home.js
+++ recipe-app/src/Pages/HomePage/Home.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import chickPeasImg from '../StockImages/chickpeas.jpg'
+import Ingredients from '../StockImages/Ingredients.jpg'
 import "./Home.css";
 
 
 const Home = () => {
-return(
-  
-  <h1>Home</h1>
-); 
-  
-} 
+
+  return (
+    <div className="HomePageFormatting">
+      <h1 className="introText">Why Us?</h1>
+      <div>
+        <div className="stockPhotos">
+          <img className="chickPeas" src={chickPeasImg}></img>
+          <img className="ingredients" src={Ingredients}></img>
+        </div>
+
+      </div>
+      <div class="text-center-card__content">
+        <div class="card__content-inner">                     
+          <h4 class="nutritonalHeading">NUTRITIONAL LAYOUT</h4>                      
+          <p class="p2-card__description">Our meals and products are backed by cutting-edge nutrition science and traditional healing wisdom to give your body what it needs to thrive.</p>
+        </div>
+      </div>
+      
+    </div>
+
+
+  );
+};
 
 export default Home;

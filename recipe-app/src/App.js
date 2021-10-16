b9baa003080c8a4cb8ed7d4c7e3e0acc2f6fa04d
EmilianoHuerta <70651959+EmilianoHuerta@users.noreply.github.com>
2021-10-16 18:26:22 -0500

    implementing helper methods for displaying current date and time

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,10 +1,11 @@
 import Sidebar from "./SideBar/Sidebar";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Pages/HomePage/Home'
 import Calendar from './Pages/CalendarPage/Calendar';
 import Recipes from './Pages/RecipesPage/Recipes';
 import ProfileDiet from './Pages/Profile/ProfileDiet';
 import ProfileIntolerances from './Pages/Profile/ProfileIntolerances';
+import DateTime from './HelperMethods/DateTime'
 
 import SpoonacularApi from './spoonacular';
 import Checkbox from './Pages/Profile/Checkbox';
@@ -13,18 +14,20 @@ import Checkbox from './Pages/Profile/Checkbox';
 
 
 function App() {
+
   return (
     <>
       <Router>
+        <DateTime/>
         <Sidebar/>
-        <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/Calendar' exact component={Calendar}/>
-          <Route path='/Recipes' exact component={Recipes}/>
-          <Route path='/ProfileDiet' exact component={ProfileDiet}/> 
-          <Route path='/ProfileIntolerances' exact component={ProfileIntolerances}/>
-        </Switch>
-      </Router>
+            <Switch>
+              <Route path='/Home' exact component={Home}/>
+              <Route path='/Calendar' exact component={Calendar}/>
+              <Route path='/Recipes' exact component={Recipes}/>
+              <Route path='/ProfileDiet' exact component={ProfileDiet}/> 
+              <Route path='/ProfileIntolerances' exact component={ProfileIntolerances}/>
+            </Switch>
+        </Router>
     </>
   );
 }

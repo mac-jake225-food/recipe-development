3becc8073b1d4a312cd85abaf20b519cbfe3f376
Emiliano Huerta <70651959+EmilianoHuerta@users.noreply.github.com>
2021-10-16 20:44:14 -0500

    attempts at fixing spacing orientation on page

--- recipe-app/src/Pages/HomePage/Home.js
+++ recipe-app/src/Pages/HomePage/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import chickPeasImg from '../StockImages/chickpeas.jpg'
 import Ingredients from '../StockImages/Ingredients.jpg'
+import Planner from '../StockImages/planner.jpg'
 import "./Home.css";
 
 
@@ -13,17 +14,19 @@ const Home = () => {
         <div className="stockPhotos">
           <img className="chickPeas" src={chickPeasImg}></img>
           <img className="ingredients" src={Ingredients}></img>
+          <img className="planner" src={Planner}></img>
         </div>
-
       </div>
-      <div class="text-center-card__content">
         <div class="card__content-inner">                     
           <h4 class="nutritonalHeading">NUTRITIONAL LAYOUT</h4>                      
+          <p class="p2-card__description">Our meals and products are backed by cutting-edge nutrition science and traditional healing wisdom to give your body what it needs to thrive.</p>      
+          </div>
+          <div class="card__content-inner-1">                     
+          <h4 class="nutritonalHeading">NUTRITIONAL LAYOUT</h4>                      
           <p class="p2-card__description">Our meals and products are backed by cutting-edge nutrition science and traditional healing wisdom to give your body what it needs to thrive.</p>
+          
+          </div>
         </div>
-      </div>
-      
-    </div>
 
 
   );

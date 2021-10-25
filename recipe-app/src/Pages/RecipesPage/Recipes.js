32bd9049dde1960e552fd65db4797fbb7ca17bf2
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2021-10-25 09:45:49 -0500

    figured out how to access data within function

--- recipe-app/src/Pages/RecipesPage/Recipes.js
+++ recipe-app/src/Pages/RecipesPage/Recipes.js
@@ -6,6 +6,7 @@ import {cuisines} from '../Profile/ProfileCuisine'
 
 class Recipes extends Component{
 
+
 searchRecipes = () =>{
   var api = new SpoonacularApi.RecipesApi()
   var opts = {
@@ -13,15 +14,26 @@ searchRecipes = () =>{
     'intolerances' : intolerances.toString(),
     'cuisine' : cuisines.toString()
   };
-
   var callback = function(error, data, response) {
     if (error) {
       console.error(error);
     } else {
-      console.log('API called successfully. Returned data: ', data);
+      // console.log('API called successfully. Returned data: ', data);
+      var recipe1= data.results[1]
+      var title = data.results[1].title
+      console.log("first recipe title", data.results[1].title)
+      console.log("second recipe title", data.results[2].title)
+
     }
   };
+
+  let data = null
   var filteredRecipes = api.searchRecipes(opts, callback)
+  console.log('API called successfully. Returned data: ', data);
+  // console.log("placeholder", placeholder)
+
+  // console.log(recipeString)
+  // console.log("testing stringify",JSON.stringify(filteredRecipes))
 }
 
 
@@ -41,6 +53,9 @@ render(){
       Generate Recipes
     </button>
     <h1>Recipe</h1>
+
+    <div>This is some text in a div element.</div>
+    
   </div>
 );}
 

efbe8ad1ba2c2c8f0c48dac7f474620d63e80b6a
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2021-11-04 21:51:58 -0500

    Deleted commented out guidance code

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -39,22 +39,3 @@ function App() {
 
 export default App; 
 
-
-// move this code where it belongs
-// testing spoonacular works
-// console.log("-----------------------> running API query");
-// var api = new SpoonacularApi.IngredientsApi()
-// var opts = {
-//   'query': "burger", // {String} The (natural language) search query.
-//   '_number': 20, // {Number} The maximum number of items to return (between 1 and 100). Defaults to 10.
-//   'metaInformation': false, // {Boolean} Whether to return more meta information about the ingredients.
-//   'intolerances': "egg" // {String} A comma-separated list of intolerances. All recipes returned must not contain ingredients that are not suitable for people with the intolerances entered. See a full list of supported intolerances.
-// };
-// var callback = function(error, data, response) {
-//   if (error) {
-//     console.error(error);
-//   } else {
-//     console.log('API called successfully. Returned data: ', data);
-//   }
-// };
-// api.autocompleteIngredientSearch(opts, callback);

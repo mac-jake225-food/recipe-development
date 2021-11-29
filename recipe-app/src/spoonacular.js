c950c02c2e9818cbc766765361452db87ccb53f2
abigailstolp <astolp@macalester.edu>
2021-11-29 12:02:39 -0600

    new API key

--- recipe-app/src/spoonacular.js
+++ recipe-app/src/spoonacular.js
@@ -6,6 +6,6 @@ console.log("--------------> spoonacular.js included")
 var SpoonacularApi = require('./vendor/spoonacular_api/src/com.spoonacular.client/index.js');
 var defaultClient = SpoonacularApi.ApiClient.instance;
 var apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
-apiKeyScheme.apiKey = "414648cecf9e476f80cfd25a256f8ba5"; 
+apiKeyScheme.apiKey = "c37d0e1412a04f4a80c38234184d07bd"; 
 
 export default SpoonacularApi;

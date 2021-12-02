2b521d5f6f377f2d207045735e068fdca15e4f9a
EmilianoHuerta <70651959+EmilianoHuerta@users.noreply.github.com>
2021-12-01 20:44:56 -0600

    Update spoonacular.js

--- recipe-app/src/spoonacular.js
+++ recipe-app/src/spoonacular.js
@@ -6,6 +6,6 @@ console.log("--------------> spoonacular.js included")
 var SpoonacularApi = require('./vendor/spoonacular_api/src/com.spoonacular.client/index.js');
 var defaultClient = SpoonacularApi.ApiClient.instance;
 var apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
-apiKeyScheme.apiKey = "c37d0e1412a04f4a80c38234184d07bd"; 
+apiKeyScheme.apiKey = "079e37d0b94a4e05a4e60c03aa6a039e"; 
 
 export default SpoonacularApi;

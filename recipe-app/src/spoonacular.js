887295cca7a0807ba44e822aab3580611132d0ba
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2021-12-03 00:10:10 -0600

    changed api key

--- recipe-app/src/spoonacular.js
+++ recipe-app/src/spoonacular.js
@@ -6,6 +6,6 @@ console.log("--------------> spoonacular.js included")
 var SpoonacularApi = require('./vendor/spoonacular_api/src/com.spoonacular.client/index.js');
 var defaultClient = SpoonacularApi.ApiClient.instance;
 var apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
-apiKeyScheme.apiKey = "079e37d0b94a4e05a4e60c03aa6a039e"; 
+apiKeyScheme.apiKey = "414648cecf9e476f80cfd25a256f8ba5"; 
 
 export default SpoonacularApi;

9ab1b6f1bfd6e384719391d0398f603fc8696ed0
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2021-12-05 13:57:32 -0600

    printed grocery list to screen, deleted excess code

--- recipe-app/src/Pages/GroceryList/GroceryList.js
+++ recipe-app/src/Pages/GroceryList/GroceryList.js
@@ -5,21 +5,10 @@ import {intolerances} from '../Profile/ProfileIntolerances'
 import {cuisines} from '../Profile/ProfileCuisine'
 import womanCooking from '../StockImages/womanCooking.jpeg'
 import { savedRecipes } from "../RecipesPage/Recipes";
-
 import { Link } from "react-router-dom";
 
 
 class GroceryList extends Component{
-  //Maybe move to recipesFile 
-  // getRecipeIngredients()
-  // getIngredientNames()
-  // state = {
-  //   ingredientList: []
-  // };
-
-  // this.setState(prevState => ({
-  //   arrayvar: [...prevState.arrayvar, newelement]
-  // }))
 
   constructor(props) {
     super(props);
@@ -30,20 +19,10 @@ class GroceryList extends Component{
 
   componentDidMount() {  
     this.getRecipeIngredients()
-    // this.getIngredientNames()
   }
   componentWillUnmount() {  
 
   }
-
-  // changeListFilled = (bool) => {
-  //   this.getRecipeIngredients()
-  //   this.getIngredientNames()
-  //   console.log('state function works')
-  //   this.setState({
-  //     listFilled : bool
-  //   });
-  // }
   changeState=(recipeName)=>{
     this.setState(prevState => ({
       ingredientList: [...prevState.ingredientList, recipeName]
@@ -56,33 +35,20 @@ class GroceryList extends Component{
         console.log("-----------------------> running API query");
         var id=savedRecipes[i].id
         var callback = function(error, data, response) {
-          //I think this callback function is what is making the list be filled as the last step
           if (error) {
             console.error(error);
           } else {
             console.log('ingredient api called successfully. Returned data: ', data.ingredients);
             for(var i=0; i<data.ingredients.length; i++){
-              var recipeName = data.ingredients[i].name
-              // ingredientList.push(recipeName)
-              // this.setState(prevState => ({
-              //   ingredientList: [...prevState.ingredientList, recipeName]
-              // }))
+              var recipeName = ' '+data.ingredients[i].name 
               this.changeState(recipeName)
             }
           }
-          // console.log('inside api call, all ingredients: ', this.state.ingredientList.toString())
-          
         };
         api.getRecipeIngredientsByID(id, callback.bind(this))
-        // this.changeListFilled.bind(null, true)
       }
     }
   }
-
-  // getIngredientNames = () => {
-  //   console.log("printing all ingredientList: ")
-  //   console.log('all ingredients: ', this.state.ingredientList.toString())
-  // }
 
 render(){  
   
@@ -96,11 +62,11 @@ render(){
     }}
   >
   <div>
-      {/* {this.getRecipeIngredients()} */}
-      {/* {this.getIngredientNames()} */}
-      {/* {console.log('list filled? ', this.state.listFilled)} */}
-      {typeof ingredientList!=undefined && this.state.ingredientList && 'works'}
       {console.log('ingredient list, in render: ', this.state.ingredientList.toString())}
+      
+      <div>
+        {this.state.ingredientList.map(txt => <p>{txt}</p>)}
+    </div>
   </div>
 
   </div>

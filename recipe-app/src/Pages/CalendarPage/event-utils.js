218e78a2c664f20d4ba6d32bbcc8a5ff16933225
EmilianoHuerta <70651959+EmilianoHuerta@users.noreply.github.com>
2021-12-14 17:36:39 -0600

    updating with comments

--- recipe-app/src/Pages/CalendarPage/event-utils.js
+++ recipe-app/src/Pages/CalendarPage/event-utils.js
@@ -1,12 +1,19 @@
 import { render } from 'react-dom'
 import {savedRecipes, intializeData } from '../RecipesPage/Recipes'
 
-
+/**
+ * this varibale is a place holder which is used in updateCalendarData to initialize the event objects for movable events on calendar
+ */
 let existingEvent = [] 
 
+/**
+ * This method takes in the current calendar data from Recipe.js and savedRecipe api call and then maps eachs value in the API in order to create event objects that are 
+ * redable by the calendar
+ * @param {currentCalendarData} currentCalendarData -- the calendar data present  
+ * @param {savedRecipes} savedRecipes 
+ * @returns 
+ */
 export function updateCalendarData(currentCalendarData, savedRecipes){
-  //for each saved recipe, find existing calendar data
-  //use start date, or make new one if it doesnt exist
   let defaultDate = new Date().toISOString().replace(/T.*$/, '') + ' 12:00:00' // YYYY-MM-DD of today
   
   return savedRecipes.map(recipe => {
@@ -19,11 +26,18 @@ export function updateCalendarData(currentCalendarData, savedRecipes){
   });
 }
 
-export function getCalendarData(){
-  savedEvents = updateCalendarData(savedEvents, savedRecipes);
-  return savedEvents;
-}
+  /**
+   * This method calls updateCalendar in order to update savedEvents to the current most updated savedRecipes 
+   * @returns savedEvents { calendar object }
+   */
+  export function getCalendarData(){
+    savedEvents = updateCalendarData(savedEvents, savedRecipes);
+    return savedEvents;
+  }
 
-let savedEvents = updateCalendarData(existingEvent, savedRecipes)
+  /**
+   * this variable update savedEvents in order to update it with the most current savedRecipes 
+   */
+  let savedEvents = updateCalendarData(existingEvent, savedRecipes)
 
 export {savedRecipes, savedEvents}
